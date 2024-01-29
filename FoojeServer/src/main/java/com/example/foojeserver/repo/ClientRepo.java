c420cd7d7da31fa5e806bb1a6ef62b03618f6a53
Karol_Bagiński <67123161+KarBagi@users.noreply.github.com>
2024-01-29 18:19:12 +0100

    Delete FoojeServer directory

--- FoojeServer/src/main/java/com/example/foojeserver/repo/ClientRepo.java
+++
@@ -1,8 +0,0 @@
-package com.example.foojeserver.repo;
-
-import com.example.foojeserver.model.Client;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ClientRepo extends JpaRepository<Client, Long> {
-    Client findByEmail(String email);
-}
